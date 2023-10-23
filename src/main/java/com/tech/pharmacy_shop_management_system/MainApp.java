2fa8edcbcb0a716b3e7d31c4403ff30eb296a997
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2023-10-23 16:16:29 +0530

    Developed the main frame with necessary parts

--- src/main/java/com/tech/pharmacy_shop_management_system/MainApp.java
+++ src/main/java/com/tech/pharmacy_shop_management_system/MainApp.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("MainFrame.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1133, 597);
+        stage.setTitle("Pharmacy Management System");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/tech/pharmacy_shop_management_system/MainAppController.java
@@ -0,0 +1,11 @@
+package com.tech.pharmacy_shop_management_system;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainAppController {
+
+
+    }
+
+
