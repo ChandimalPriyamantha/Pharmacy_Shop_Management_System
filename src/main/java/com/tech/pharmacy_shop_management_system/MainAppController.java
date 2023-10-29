e7b0a78c3c4f877cf6271a6928aebbf55c77092c
GANIDU2413 <ganidusahan@gmail.com>
2023-10-29 17:15:51 +0530

    1715291023

--- src/main/java/com/tech/pharmacy_shop_management_system/MainAppController.java
+++ src/main/java/com/tech/pharmacy_shop_management_system/MainAppController.java
@@ -10,6 +10,19 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 public class MainAppController {
+    @FXML
+    private Button inrbtn;
+
+    @FXML
+    private AnchorPane inventoryAP;
+
+    @FXML
+    private AnchorPane reportAP;
+        public void inventoryAndReport(ActionEvent event) throws SQLException {
+            if (event.getSource() == inrbtn){
+                inventoryAP.setVisible(true);
+            }
+        }
 
 
     }
