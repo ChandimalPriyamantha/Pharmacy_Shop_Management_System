956ba845e7458290de714fb26782d71ffbeec6b0
TMadhusha <122914674+TMadhusha@users.noreply.github.com>
2023-11-03 19:50:18 +0530

    LogIn

---
+++ src/main/java/Authourization/LogInController.java
@@ -0,0 +1,30 @@
+package Authourization;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class LogInController {
+    @FXML
+    private AnchorPane main;
+
+    @FXML
+    private AnchorPane title_button_tab;
+
+    @FXML
+    private Button adminbtn;
+
+    @FXML
+    private Button userbtn;
+
+    @FXML
+    private AnchorPane Admin_panel;
+    @FXML
+    private AnchorPane User_panel;
+
+
+
+
+
+}
