7ea086bb867d4ca51e90a7d7af330eaca1bd2d6a
TMadhusha <122914674+TMadhusha@users.noreply.github.com>
2023-11-05 17:04:30 +0530

    updated

--- src/main/java/com/tech/pharmacy_shop_management_system/LogInController.java
+++ src/main/java/com/tech/pharmacy_shop_management_system/LogInController.java
@@ -3,14 +3,11 @@ package com.tech.pharmacy_shop_management_system;
 import Connection.DatabaseConnection;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -51,15 +48,44 @@ public class LogInController {
     @FXML
     private AnchorPane User_panel;
     @FXML
-    private Label Uuname;
+    private TextField Uuname;
     @FXML
-    private Label Upwd;
+    private PasswordField Upwd;
     @FXML
     private Button Ulogin;
     @FXML
     private Button Ufpwd;
     @FXML
     private Button Ureg;
+
+    @FXML
+    private AnchorPane adreg_panel;
+    @FXML
+    private TextField adreguname;
+    @FXML
+    private PasswordField adregpwd;
+    @FXML
+    private ComboBox adregcom;
+    @FXML
+    private TextField adans;
+    @FXML
+    private Button adregbtn;
+    @FXML
+    private Button adlogbtn;
+    @FXML
+    private AnchorPane ureg_panel;
+    @FXML
+    private TextField streguname;
+    @FXML
+    private PasswordField stregpwd;
+    @FXML
+    private ComboBox stregcom;
+    @FXML
+    private TextField stans;
+    @FXML
+    private Button stregbtn;
+    @FXML
+    private Button stlogbtn;
 
     @FXML
     public void controlPanel(ActionEvent event){
@@ -124,6 +150,49 @@ public class LogInController {
             stage.setScene(mainFrame);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void UloginOnAction(ActionEvent event) {
+        connect=DatabaseConnection.ConnectionDB();
+
+        String userun=Uuname.getText();
+        String userpwd=Upwd.getText();
+        try {
+            String sql = "select userID,password from staff where userID='"+userun+"' and password='"+userpwd+"'";
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            if (result.next()) {
+                if (userun.isEmpty() || userpwd.isEmpty()) {
+                    showAlert("Please fill out all the fields...!");
+                }
+                else {
+                    showAlert("You are logged in successfully....!");
+                    try {
+                        gotoMainFrame(event);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                showAlert("Username or Password is incorrect. Please try again...");
+                Uuname.setText(" ");
+                Upwd.setText(" ");
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            System.out.println("An SQL error occurred. Please check the console for details.");
+        }
+    }
+
+    @FXML
+    public void controlpanel(ActionEvent event){
+        if(event.getSource()==areg){
+            Admin_panel.setVisible(false);
+
         }
     }
 
